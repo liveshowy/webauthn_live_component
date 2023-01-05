e60939ee78700d38cdcbd3f3bfe4cbdd60551a56
Owen Bickford <owen@owencode.com>
2023-01-04 21:45:34 -0600

    update support component docs

--- lib/webauthn_components/support_component.ex
+++ lib/webauthn_components/support_component.ex
@@ -2,18 +2,33 @@ defmodule WebauthnComponents.SupportComponent do
   @moduledoc """
   A LiveComponent for detecting WebAuthn support.
 
+  This component should be used in combination with `RegistrationComponent` and `AuthenticationComponent` to disable their buttons when WebAuthn is not supported or enabled.
+
+  An application may also use `SupportComponent` to steer users away from traditional authentication to the more secure Passkey authentication method. For example, an application that supports both traditional authentication and Passkeys may redirect users to a Passkey LiveView or render a message encouraging the new authentication method.
+
   See [USAGE.md](./USAGE.md) for example code.
 
   ## Assigns
 
+  - `@passkeys_supported`: (Internal) A boolean indicating the client does or does not support the WebAuthn API.
+
   ## Events
 
+  - `"passkeys-supported"`: Sent by the client when Passkey support has been checked.
+
   ## Messages
+
+  - `{:passkeys_supported, boolean}`
+    - `boolean` will be `true` when WebAuthn is supported and enabled, and `false` when it is not supported or enabled.
   """
   use Phoenix.LiveComponent
 
   def mount(socket) do
-    {:ok, socket}
+    {
+      :ok,
+      socket
+      |> assign(:passkeys_supported, nil)
+    }
   end
 
   def render(assigns) do
