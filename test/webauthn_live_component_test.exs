cc765f9839c17402a54f6ef6000a760fa4a4beac
Owen Bickford <owen@owencode.com>
2023-01-04 23:09:16 -0600

    rm old tests

--- test/webauthn_live_component_test.exs
+++ test/webauthn_live_component_test.exs
@@ -3,20 +3,4 @@ defmodule WebauthnComponentsTest do
   use ComponentCase
   import Phoenix.LiveView.Helpers
   doctest WebauthnComponents
-
-  test "socket assigns include a valid changeset on mount" do
-    socket = %Phoenix.LiveView.Socket{}
-    assert {:ok, component} = WebauthnComponents.mount(socket)
-    changeset = component.assigns.changeset
-    assert %Ecto.Changeset{} = changeset
-    assert Enum.empty?(changeset.errors)
-    assert changeset.valid?
-  end
-
-  test "a form is rendered" do
-    live_component = live_component(WebauthnComponents, id: "test_form")
-    assert %Phoenix.LiveView.Component{component: component, assigns: assigns} = live_component
-    assert %Phoenix.LiveView.Rendered{} = component.render(assigns)
-    assert render_component(WebauthnComponents, id: "test_form") =~ "username"
-  end
 end
