84cd1b95c20b5f98a5a74b3cb037357d075b9bd7
Owen Bickford <owen@owencode.com>
2023-01-08 16:43:56 -0600

    start test endpoint in component case, add socket to setup

--- test/support/component_case.ex
+++ test/support/component_case.ex
@@ -2,14 +2,21 @@ defmodule ComponentCase do
   @moduledoc false
   use ExUnit.CaseTemplate
 
-  setup do
+  setup_all do
+    TestEndpoint.start_link()
     :ok
+  end
+
+  setup do
+    socket = %Phoenix.LiveView.Socket{}
+    %{socket: socket}
   end
 
   using do
     quote do
       import Phoenix.LiveViewTest
       import Phoenix.LiveView.Helpers
+      import LiveIsolatedComponent
     end
   end
 end
