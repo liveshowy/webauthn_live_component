3101f9c2a065e35a140d6512d158695452b09256
Owen Bickford <owen@owencode.com>
2023-01-08 21:21:40 -0600

    move endpoint start

--- test/support/component_case.ex
+++ test/support/component_case.ex
@@ -2,11 +2,6 @@ defmodule ComponentCase do
   @moduledoc false
   use ExUnit.CaseTemplate
 
-  setup_all do
-    TestEndpoint.start_link()
-    :ok
-  end
-
   setup do
     conn = Phoenix.ConnTest.build_conn()
     socket = %Phoenix.LiveView.Socket{}
